c76343280e3ebc4b9b1a0a80382601397b3f5918
Gray Smith <gray.smith00@gmail.com>
2018-09-26 10:18:00 -0600

    Add dummy data

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const http = require('http');
 const url = require('url');
 const server = http.createServer();
+
+let messages = [
+  { id: 1, user: 'brittany storoz', message: 'hi there!' },
+  { id: 2, user: 'bob loblaw', message: 'check out my law blog' },
+  { id: 3, user: 'lorem ipsum', message: 'dolor set amet' }
+];
